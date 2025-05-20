44d17e1d0b4115ed25aa961173a9647e8f58fd75
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-20 17:02:06 -0500

    Fill in LazySingleton

--- src/main/java/LazySingleton.java
+++ src/main/java/LazySingleton.java
@@ -1,7 +1,15 @@
 public class LazySingleton {
+    private static LazySingleton instance;
+    private final String id;
+
+    private LazySingleton(int i){
+        this.id = String.valueOf(i);
+    }
     public static LazySingleton getInstance(int i) {
+        return instance;
     }
 
     public String getId() {
+        return this.id;
     }
 }
