0d11ea2d855f324ad43017819af133f39ef81119
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-21 13:34:45 -0500

    Fill in EagerSingleton

--- src/main/java/EagerSingleton.java
+++ src/main/java/EagerSingleton.java
@@ -1,7 +1,16 @@
 public class EagerSingleton {
+    private static EagerSingleton instance = new EagerSingleton();
+    private final String id;
+
+    private EagerSingleton() {
+        this.id = "0";
+    }
+
     public static EagerSingleton getInstance(int i) {
+        return instance;
     }
 
     public String getId() {
+        return this.id;
     }
 }
