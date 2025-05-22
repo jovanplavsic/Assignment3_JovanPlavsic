29448a3e79d8634b4a2a07a6db81febbf186ffb0
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-22 08:58:19 -0500

    Updated EagerSingelton to have 3 instances

--- src/main/java/EagerSingleton.java
+++ src/main/java/EagerSingleton.java
@@ -1,16 +1,27 @@
 public class EagerSingleton {
-    private static EagerSingleton instance = new EagerSingleton();
-    private final String id;
+    private static EagerSingleton[] INSTANCES = new EagerSingleton[3];
+    static {
+        for (int i = 0; i < INSTANCES.length; i++) {
+            INSTANCES[i] = new EagerSingleton(i);
+        }
+    }
+    private final int id;
 
-    private EagerSingleton() {
-        this.id = "0";
+
+    private EagerSingleton(int id) {
+        this.id = id;
     }
 
-    public static EagerSingleton getInstance(int i) {
-        return instance;
+    public static EagerSingleton getInstance(int idx) {
+
+        if (idx >= 0 && idx < INSTANCES.length){
+            return INSTANCES[idx];
+        } else{
+            throw new IllegalArgumentException();
+        }
     }
 
     public String getId() {
-        return this.id;
+        return Integer.toString(this.id);
     }
 }
