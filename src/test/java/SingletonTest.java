5a0a8f31f84ec162c5e09771e95e014fd35e0dbe
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-22 09:52:18 -0500

    Make SingletonTest shell

--- src/test/java/SingletonTest.java
+++ src/test/java/SingletonTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class SingletonTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SingletonTest {
+    @Test
+    void testLazyInstantiation(){}
+
+    @Test
+    void testEagerInstantiation(){}
+
+    @Test
+    void testLazyListReturnsCorrectIdx(){}
+
+    @Test
+    void testEagerListReturnsCorrectIdx(){}
+
+    @Test
+    void testLazyIllegalArgument(){}
+
+    @Test
+    void testEagerIllegalArgument(){}
 }
