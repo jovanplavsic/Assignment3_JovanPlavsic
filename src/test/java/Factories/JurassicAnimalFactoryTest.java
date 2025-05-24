36eea15dfab9d48db218b28ffb10cbfb7d8d4bb3
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-24 12:41:55 -0500

    Add Jurrassic and Triassic tests

--- src/test/java/Factories/JurassicAnimalFactoryTest.java
+++ src/test/java/Factories/JurassicAnimalFactoryTest.java
@@ -1,24 +1,49 @@
 package Factories;
 
+import Animals.LandAnimal;
+import Animals.SeaAnimal;
+import Animals.SkyAnimal;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JurassicAnimalFactoryTest {
+private final JurassicAnimalFactory factory  = new JurassicAnimalFactory();
 
     @Test
     void getEra() {
+        assertEquals("Jurassic", factory.getEra());
     }
 
     @Test
     void createLandAnimals() {
+        List<LandAnimal> a = factory.createLandAnimals();
+
+        assertEquals(2, a.size());
+
+        assertTrue(a.stream().anyMatch(x -> x.getName().equals("Stegosaurus")));
+        assertTrue(a.stream().anyMatch(x -> x.getName().equals("Allosaurus")));
     }
 
     @Test
     void createSeaAnimals() {
+        List<SeaAnimal> b = factory.createSeaAnimals();
+
+        assertEquals(2, b.size());
+
+        assertTrue(b.stream().anyMatch(x -> x.getName().equals("Liopleurodon")));
+        assertTrue(b.stream().anyMatch(x -> x.getName().equals("Plesiosaurus")));
     }
 
     @Test
     void createSkyAnimals() {
+        List<SkyAnimal> c = factory.createSkyAnimals();
+
+        assertEquals(2, c.size());
+
+        assertTrue(c.stream().anyMatch(x -> x.getName().equals("Pterodactylus")));
+        assertTrue(c.stream().anyMatch(x -> x.getName().equals("Rhamphorhynchus")));
     }
 }
--- src/test/java/Factories/TriassicAnimalFactoryTest.java
+++ src/test/java/Factories/TriassicAnimalFactoryTest.java
@@ -1,24 +1,49 @@
 package Factories;
 
+import Animals.LandAnimal;
+import Animals.SeaAnimal;
+import Animals.SkyAnimal;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriassicAnimalFactoryTest {
+private final TriassicAnimalFactory factory  = new TriassicAnimalFactory();
 
     @Test
     void getEra() {
+        assertEquals("Triassic", factory.getEra());
     }
 
     @Test
     void createLandAnimals() {
+        List<LandAnimal> a = factory.createLandAnimals();
+
+        assertEquals(2, a.size());
+
+        assertTrue(a.stream().anyMatch(x -> x.getName().equals("Plateosaurus")));
+        assertTrue(a.stream().anyMatch(x -> x.getName().equals("Coelophysis")));
     }
 
     @Test
     void createSeaAnimals() {
+        List<SeaAnimal> b = factory.createSeaAnimals();
+
+        assertEquals(2, b.size());
+
+        assertTrue(b.stream().anyMatch(x -> x.getName().equals("Tanystropheus")));
+        assertTrue(b.stream().anyMatch(x -> x.getName().equals("Nothosaurus")));
     }
 
     @Test
     void createSkyAnimals() {
+        List<SkyAnimal> c = factory.createSkyAnimals();
+
+        assertEquals(2, c.size());
+
+        assertTrue(c.stream().anyMatch(x -> x.getName().equals("Preondactylus")));
+        assertTrue(c.stream().anyMatch(x -> x.getName().equals("Eudimorphodon")));
     }
 }
