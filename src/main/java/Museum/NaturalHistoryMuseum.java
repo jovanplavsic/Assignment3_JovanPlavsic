7e6c1bd3f2fe8e3d902e9fe3bad92ba9b302b657
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-24 13:27:07 -0500

    Import all factories for use in museum

--- src/main/java/Museum/NaturalHistoryMuseum.java
+++ src/main/java/Museum/NaturalHistoryMuseum.java
@@ -1,9 +1,11 @@
+package Museum;
+
 import Animals.LandAnimal;
 import Animals.SeaAnimal;
 import Animals.SkyAnimal;
 import Factories.AnimalAbstractFactory;
-import Factories.JurassicAnimalFactory;
 import Factories.CenozoicAnimalFactory;
+import Factories.JurassicAnimalFactory;
 import Factories.TriassicAnimalFactory;
 
 import java.util.List;
@@ -11,7 +13,7 @@ import java.util.List;
 public class NaturalHistoryMuseum {
     public static void main(String[] args) {
         // set up the exhibit
-        AnimalAbstractFactory f = new TriassicAnimalFactory();
+        AnimalAbstractFactory f = new JurassicAnimalFactory();
         System.out.println("You are in the " + f.getEra() + " exhibition");
         System.out.println("===== " + f.getEra() + " land animals section =====");
         List<LandAnimal> landAnimals = f.createLandAnimals();
