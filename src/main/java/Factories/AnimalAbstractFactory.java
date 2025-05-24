993af91d88cb6b97eee2858721a6d48f1ba745e1
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-24 13:38:42 -0500

    Annotate AnimalAbstract

--- src/main/java/Factories/AnimalAbstractFactory.java
+++ src/main/java/Factories/AnimalAbstractFactory.java
@@ -4,14 +4,36 @@ import Animals.LandAnimal;
 import Animals.SeaAnimal;
 import Animals.SkyAnimal;
 
+/**
+ * Defines a factory for grouping animals by its era
+ * Each factory knows its own era and methods to create land, sea, and sky animal exhibits.
+ */
 import java.util.List;
 
 public interface AnimalAbstractFactory {
+    /**
+     * Return the era it represents
+     */
     String getEra();
 
+    /**
+     * Creates a list of land animals from the that era
+     *
+     * @return a List of {@link LandAnimal} instances
+     */
     public List<LandAnimal> createLandAnimals();
 
+    /**
+     * Creates a list of sea animals from the that era
+     *
+     * @return a List of {@link SeaAnimal} instances
+     */
     public List<SeaAnimal> createSeaAnimals();
 
+    /**
+     * Creates a list of sky animals from the that era
+     *
+     * @return a List of {@link SkyAnimal} instances
+     */
     public List<SkyAnimal> createSkyAnimals();
 }
