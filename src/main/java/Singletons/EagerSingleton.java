381d82d5b426acb6cecb0490d5e0cca59228ee1b
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-24 13:25:01 -0500

    Annotate Eager and Lazy Singleton classes with JavaDoc

--- src/main/java/Singletons/EagerSingleton.java
+++ src/main/java/Singletons/EagerSingleton.java
@@ -1,29 +1,56 @@
 package Singletons;
 
+/**
+ * Implements Eager Singleton pattern for only 3 instances. All instances are created when class is loaded.
+ */
 public class EagerSingleton {
+
+    /**
+     * Array of instances holds a max of 3 instances
+     */
     private static EagerSingleton[] INSTANCES = new EagerSingleton[3];
+
+    /** Instantiates all three instances when class is loaded */
     static {
         for (int i = 0; i < INSTANCES.length; i++) {
             INSTANCES[i] = new EagerSingleton(i);
             System.out.println("Eager Singleton " + i + " instantiated");
         }
     }
+
+    /**
+     * Unique identifier for each instance
+     */
     private final String id;
 
-
+    /**
+     * Instantiates an eager singleton
+     *
+     * @param id is the unique identifier for that instance
+     */
     private EagerSingleton(int id) {
         this.id = Integer.toString(id);
     }
 
+    /**
+     * Returns an instance dependingon it's index in the list which corresponds with its id
+     *
+     * @param idx is the identifier and location and list
+     * @return an EagerSingleton object
+     * @throws IllegalArgumentException if idx is outside of possible locations
+     */
     public static EagerSingleton getInstance(int idx) {
 
-        if (idx >= 0 && idx < INSTANCES.length){
+        if (idx >= 0 && idx < INSTANCES.length) {
             return INSTANCES[idx];
-        } else{
+        } else {
             throw new IllegalArgumentException();
         }
     }
 
+    /**
+     * Returns the id for this eager singleton instance
+     */
     public String getId() {
         return this.id;
     }
--- src/main/java/Singletons/LazySingleton.java
+++ src/main/java/Singletons/LazySingleton.java
@@ -1,13 +1,36 @@
 package Singletons;
 
+/**
+ * Implements Lazy Singleton pattern for only 3 instances. All instances are created only when first called
+ */
 public class LazySingleton {
+    /**
+     * Array of instances holds a max of 3 instances
+     */
     private static LazySingleton[] INSTANCES = new LazySingleton[3];
+
+    /**
+     * Unique identifier for each instance
+     */
     private final String id;
 
-    private LazySingleton(int i) {
-        this.id = String.valueOf(i);
+    /**
+     * Instantiates a lazy singleton
+     *
+     * @param id is the unique identifier for that instance
+     */
+    private LazySingleton(int id) {
+        this.id = String.valueOf(id);
     }
 
+    /**
+     * Returns an instance depending on it's index in the list which corresponds with its id
+     * If id is within bounds of list but has no singleton, the singleton is instantiated and returned
+     *
+     * @param idx is the identifier and location and list
+     * @return an LazySingleton object
+     * @throws IllegalArgumentException if idx is outside of list bounds
+     */
     public static LazySingleton getInstance(int idx) {
         if (idx < 0 || idx >= INSTANCES.length) {
             throw new IllegalArgumentException();
@@ -19,6 +42,9 @@ public class LazySingleton {
         return INSTANCES[idx];
     }
 
+    /**
+     * Returns the id for this singleton instance
+     */
     public String getId() {
         return this.id;
     }
